ea7dde05928fa128d0c3ab5cd92d32d53b1ea85f
felixarvid2 <felixarvid@gmail.com>
2026-09-04 14:58:52 +0200

    pddatering med mer kod

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 x = 10
-for n in range(x):
-    print(n)
+y = 5
+num = [1, 2, 3, 4, 5]
+
+
+print(num)
+
+
+ids = [1, 2, 3]
+todo = {"id": 1, "task": "chilla hela dagen"}
+task = {"id": 1, "task": "chilla hela dagen"}
+todos = [
+    {"id": 1, "task": "chilla hela dagen"},
+    {"id": 2, "task": "åka tåg"}
+    ]
+
+print("saker jag behöver göra:\n")
+for todo in todos:
+    print(f"{todo["id"]}. {todo["task"]}")
